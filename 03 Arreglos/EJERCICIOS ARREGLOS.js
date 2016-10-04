a3943bc48248ec091858473a0f79f0d8eeace980
vaRinia <varinia.ep@gmail.com>
2016-10-04 08:23:58 -0500

    avance

--- 03 Arreglos/EJERCICIOS ARREGLOS.js
+++ 03 Arreglos/EJERCICIOS ARREGLOS.js
@@ -1,54 +1,159 @@
-Ejercicio #1: Calculando DNI
+﻿------------------------------<<<
+//Ejercicio #1: Calculando DNI
+/*El cálculo de la letra del Documento Nacional de Identidad (DNI) es un proceso matemático sencillo que se basa en 
+obtener el residuo de la división entera del número de DNI entre el número 23.
 
-El cálculo de la letra del Documento Nacional de Identidad (DNI) es un proceso matemático sencillo que se basa en obtener el resto de la división entera del número de DNI y el número 23.
-
-A partir del resto de la división, se obtiene la letra seleccionándola dentro de un array de letras.
+A partir del residuo de la división, se obtiene la letra seleccionándola dentro de un array de letras.
 
 El array de letras es:
 
 var letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E', 'T'];
 
-Por tanto si el resto de la división es 0, la letra del DNI es la T y si el resto es 3 la letra es la A. Con estos datos, elaborar un pequeño script que:
+Por tanto si el residuo de la división es 0, la letra del DNI es la T y si el residuo es 3 la letra es la A. 
+Con estos datos, elaborar un pequeño script que:
 
-Almacene en una variable el número de DNI indicado por el usuario y en otra variable la letra del DNI que se ha indicado. (Pista: si se quiere pedir directamente al usuario que indique su número y su letra, se puede utilizar la función prompt())
-En primer lugar (y en una sola instrucción) se debe comprobar si el número es menor que 0 o mayor que 99999999. Si ese es el caso, se muestra un mensaje al usuario indicando que el número proporcionado no es válido y el programa no muestra más mensajes.
+Almacene en una variable el número de DNI indicado por el usuario y en otra variable la letra del 
+DNI que se ha indicado. (Pista: si se quiere pedir directamente al usuario que indique su número y su letra, se puede utilizar la función prompt())
+
+En primer lugar (y en una sola instrucción) se debe comprobar si el número es menor que 0 o mayor que 99999999. 
+Si ese es el caso, se muestra un mensaje al usuario indicando que el número proporcionado no es válido y el programa no muestra más mensajes.
+
 Si el número es válido, se calcula la letra que le corresponde según el método explicado anteriormente.
-Una vez calculada la letra, se debe comparar con la letra indicada por el usuario. Si no coinciden, se muestra un mensaje al usuario diciéndole que la letra que ha indicado no es correcta.
+
+Una vez calculada la letra, se debe comparar con la letra indicada por el usuario. Si no coinciden, se muestra un mensaje al usuario diciéndole 
+que la letra que ha indicado no es correcta.
 En otro caso, se muestra un mensaje indicando que el número y la letra de DNI son correctos.
+*/
+
+var letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E', 'T'];
+
+var numeroDni = prompt("Indique el número de DNI");
+numeroDni = Number(numeroDni);
+var letraDni = prompt("Indique la letra asignada ", "MAYÚSCULA");
+
+
+if ((numeroDni < 0) || (numeroDni > 99999999)) { 
+	alert("el número proporcionado no es válido");
+	} else {
+		var dividido = numeroDni / 23;
+		if (letraDni == letras[dividido]){
+			alert("El número y la letra DNI son correctos");
+		} else {
+			alert("La letra no es correcta");
+		}
+
+	}
 
 
 
+-----------------------<<
+//Ejercicio #2: Añadir elementos a un array con push (Ciclo for)
+//Crear un array de elementos que contenga nombres y posteriormente agregar más elementos por medio del método “push”
+
+var i = 0;
+var nombres = [];
+while (nomb !="0") {
+	var nomb = prompt("Ingrese el nombre");
+	if (nomb !="0") {
+		nombres.push(nomb);
+		i++;
+	}
+}
+alert("Los nombres son: " + i + ": " + nombres);
+
+***********
+var nombres = [];
+for (var i = -1; nomb != "0"; i++ ) {
+	var nomb = prompt("Ingrese el nombre");
+	if (nomb !="0") {
+		nombres.push(nomb);
+		i++;
+	}
+}
+alert("Los nombres son: " + i + ": " + nombres);
+
+---------------------------<<
+//Ejercicio #3: Creando arrays
+/* Crear un array llamado meses y que almacene el nombre de los doce meses del año. 
+Mostrar por pantalla los doce nombres utilizando la función alert()*/
+
+var meses = ["enero" , "febrero" , "marzo" ,  "abril" , "mayo" , "junio" , "julio" ,  "agosto" , "septiembre" , "octubre" , "noviembre" , "diciembre"];
+var i = 1;
+for (var i = 0; i < 12; i++) {
+	alert(meses[i]);
+}
 
 
-Ejercicio #2: Añadir elementos a un array con push(Ciclo for)
 
-Crear un array de elementos que contenga nombres y posteriormente agregar más elementos por medio del método “push”
-
-
-
-
-
-Ejercicio #3: Creando arrays
-
-Crear un array llamado meses y que almacene el nombre de los doce meses del año. Mostrar por pantalla los doce nombres utilizando la función alert()
-
-
-
-
-
-Ejercicio #4: Operaciones en arrays
-
-A partir del siguiente array que se proporciona:
+------------------------------<<
+//Ejercicio #4: Operaciones en arrays
+/*A partir del siguiente array que se proporciona:
+var valores = [true, 5, false, "hola", "adios", 2];
+*/
 
 var valores = [true, 5, false, "hola", "adios", 2];
 
-Determinar cuál de los dos elementos de texto es mayor
-Utilizando exclusivamente los dos valores booleanos del array, determinar los operadores necesarios para obtener un resultado true y otro resultado false
-Determinar el resultado de las cinco operaciones matemáticas realizadas con los dos elementos numéricos
+//Determinar cuál de los dos elementos de texto es mayor 
+if (valores[3].length > valores[4].length ) {
+	console.log(valores[3] + " tiene más letras que " + valores[4]);
+} else {
+	console.log(valores[4] + " tiene más letras que " + valores[3]);
+}
+
+//Determinar el resultado de las cinco operaciones matemáticas realizadas con los dos elementos numéricos
+console.log("Operaciones matemáticas:")
+var suma = valores[1] + valores[5];
+var resta = valores[1] - valores[5];
+var multiplicacion = valores[1] * valores[5];
+var division = valores[1] / valores[5];
+var potencia = valores[1] * valores[1];
+console.log("La suma es " + suma);
+console.log("La diferencia es " + resta);
+console.log("El producto es " + multiplicacion);
+console.log("La división es " + division);
+console.log(valores[1] + " elevado a la potencia " + valores[5] + " es " + potencia);
+
+//Utilizando exclusivamente los dos valores booleanos del array, determinar los operadores necesarios para obtener un resultado true y otro resultado false
+if (valores[0] == (6>5)) { 
+	console.log("True");
+} else {
+	console.log("false");
+}
+
+if (valores[2] == (5<6)) {
+	console.log("true");
+} else {
+	console.log("False");
+}
 
 
+-----------------------------<<
+//Ejercicio #5: Suma de números
+//Hacer un ejercicio que nos pida 10 números y luego que sume esos valores.
 
 
-Ejercicio #5: Suma de números
+/*
+¿POR QUÉ ASI NO FUNCIONA?
+var arreglo = [];
+var suma = 0;
+for (var i = 0; i < 10; i++ ) {
+	var num = prompt("Ingrese el número");
+	num = Number(num);
+	arreglo.push(num);
+}
+alert("Los números son: " + arreglo);
 
-Hacer un ejercicio que nos pida 10 números y luego que sume esos valores.
+for (var j = 0; j < 10; j++); {
+	var suma = suma + arreglo[j];
+}
+alert("La suma es: " + suma);*/
+
+var arreglo = [];
+var suma = 0;
+for (var i = 0; i < 10; i++ ) {
+	var num = prompt("Ingrese el número");
+	num = Number(num);
+	arreglo.push(num);
+	suma = suma + arreglo [i];
+}
+alert("Los números son: " + arreglo + ". La suma es: " + suma);
--- 04 Funciones/EJERCICIOS FUNCIONES.js
+++ 04 Funciones/EJERCICIOS FUNCIONES.js
@@ -1,50 +1,143 @@
-Ejercicio #1: El menor de los numeros
-
-Elaborar una función a la cual le enviemos tres enteros y muestre el menor.
-
-Ejercicio #2: Numero par o impar
-
-Escribir el código de una función a la que se pasa como parámetro un número entero y devuelve como resultado una cadena de texto que indica si el número es par o impar.
-
-Mostrar por pantalla el resultado devuelto por la función.
+﻿-------------------------------
+//Ejercicio #1: El menor de los numeros
+//Elaborar una función a la cual le enviemos tres enteros y muestre el menor.
 
 
+function mostrarMenor (a,b,c) {
+/*var a = prompt("Ingrese primer número");
+a = Number(a);
+var b = prompt("Ingrese segundo número");
+b= Number(b);
+var c = prompt("Ingrese tercer número");
+c = Number(c); */
+if (a < b) {
+	if (a < c) {
+	return a;
+	} else {
+	return c;
+	}
+} else {
+	if (b < c) {
+	return b;
+	} else {
+	return c;
+	}
+}
 
-
-Ejercicio #3: Ordenando numeros
-
-Realizar una función a la cual le envíe tres enteros y los muestre ordenados de menor a mayor.
-
-
-
-
-Ejercicio #4: Mayusculas o minusculas
-
-Definir una función que muestre información sobre una cadena de texto que se le pasa como argumento. A partir de la cadena que se le pasa, la función determina si esa cadena está formada sólo por mayúsculas, sólo por minúsculas o por una mezcla de ambas.
-
-
-
-
-
-Ejercicio #5: Palindromo
-
-Definir una función que determine si la cadena de texto que se le pasa como parámetro es un palíndromo, es decir, si se lee de la misma forma desde la izquierda y desde la derecha.
-
-Ejemplo de palíndromo complejo: "La ruta nos aporto otro paso natural".
+console.log("El número menor es " + mostrarMenor(a,b,c);
 
 
 
+--------------------------
+//Ejercicio #2: Numero par o impar
+/*Escribir el código de una función a la que se pasa como parámetro un número entero y devuelve como resultado una cadena de texto 
+que indica si el número es par o impar. 
+
+Mostrar por pantalla el resultado devuelto por la función.*/
 
 
-Ejercicio #6: Operaciones matematicas
+function decirPar (n) {
+	if ((n % 2) === 0) {
+	return "par";
+} else {
+	return "impar";	
+}
 
-Realizar un programa que te de la opción de la operación matemática que quieres realizar por medio de funciones
+console.log(decirPar(n))
 
 
 
 
+
+
+-------------------------------------
+//Ejercicio #3: Ordenando numeros.
+//Realizar una función a la cual le envíe tres enteros y los muestre ordenados de menor a mayor.
+
+function() {
+	
+}
+
+
+
+------------------------------
+//Ejercicio #4: Mayusculas o minusculas
+//Definir una función que muestre información sobre una cadena de texto que se le pasa como argumento. 
+//A partir de la cadena que se le pasa, la función determina si esa cadena está formada sólo por mayúsculas, sólo por minúsculas o por una mezcla de ambas.
+
+function() {
+	
+}
+
+
+
+
+----------------------------------------
+//Ejercicio #5: Palindromo
+/*Definir una función que determine si la cadena de texto que se le pasa como parámetro es un palíndromo, 
+es decir, si se lee de la misma forma desde la izquierda y desde la derecha.
+
+Ejemplo de palíndromo complejo: "La ruta nos aporto otro paso natural".*/
+
+
+function definirPalindromo (texto) {
+	var texto = prompt ("Introduzca el texto deseado");
+	//Quitar todos los espacios
+	//Comparar posición cero con ultima posicion
+	return si
+	return no
+}
+
+console.log("El texto introducido " + definirPalindromo + " es un palíndromo");
+
+
+
+
+
+
+
+------------------------------
+//Ejercicio #6: Operaciones matemáticas
+
+//Realizar un programa que te de la opción de la operación matemática que quieres realizar por medio de funciones
+
+
+
+
+function sumar {}
+function restar {}
+function multiplicar {}
+function dividir {}
+function elevarPotencia {}
+
+var operacion = prompt ("Introduzca la operación que quiere realizar: suma, resta, multiplicacion. division, potencia")
+if (operacion = "suma") {
+	var resultado = prompt ("Introduzca el " + i + " numero a sumar. Para salir escriba r");
+	while (resultado !== "r") {
+	resultado = resultado + resultado;
+	alert("El resultado de la suma es " + resultado);
+	i++;
+	}
+	
+} else if (operacion = "resta") {
+	var resultado = prompt ("Introduzca el " + i + " numero a restar. Para salir escriba r");
+	for (resultado = 0; resta !== r; i++) {
+	resta = resta - resta;
+	alert("El resultado de la resta es " + resta);
+	}
+} else if (operacion = "multiplicacion") {
+	var multi = prompt ("Introduzca el " + i + " numero a multiplicar. Para salir escriba r");
+	for (multi = 1; multi !== r; i++) {
+	multi = multi * multi;
+	alert("El resultado de la resta es " + resta);
+	}
+
+		
+}
+console.log("El resultado de la operacion es " + resultado);
 
 Ejercicio #7: Cuadrado de la suma de dos numeros
+
 
 Realizar un programa que calcule el cuadrado de la suma de dos números
 
@@ -52,22 +145,60 @@ Realizar un programa que calcule el cuadrado de la suma de dos números
 
 
 
-Ejercicio #8: Numero primo
-
-Realizar un programa que por medio de una función nos indique si el número que el usuario ingresa es primo o no
 
 
-
-
-
-Ejercicio #9: Multiplos de un numero
-
-Programa que calcula los 10 primeros múltiplos del número que queramos, utilizando una función que retorna parámetro.
+function sacarCuadrado (a,b) { 
+	var suma = a + b;
+	return suma * suma;
+}
+console.log(sacarCuadrado);
 
 
 
+------------------
+//Ejercicio #8: Numero primo
+
+//Realizar un programa que por medio de una función nos indique si el número que el usuario ingresa es primo o no
 
 
-Ejercicio #10: Cantidad de digitos
 
-Desarrollar una función que retorne la cantidad de dígitos que tiene una variable entera positiva.
+
+
+
+
+function revisarPrimo(a) {
+	var a prompt ("Introduzca el número");
+	a = Number(a);
+	for (i = 2; a % i != 0; i++) {
+	var divisor = i;
+	}
+	if (divisor === a) {
+	return si;
+	} else {
+	return no;}
+}
+
+
+-----------------------------------
+//Ejercicio #9: Múltiplos de un numero
+
+//Programa que calcula los 10 primeros múltiplos del número que queramos, utilizando una función que retorna parámetro.
+
+
+function() {
+	
+}
+
+
+
+
+
+-------------------------------
+//Ejercicio #10: Cantidad de digitos
+//Desarrollar una función que retorne la cantidad de dígitos que tiene una variable entera positiva.
+
+function leerDigitos (numero) {
+	numero.lenght
+}
+
+
